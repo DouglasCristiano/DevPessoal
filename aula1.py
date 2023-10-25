f12d1c18cc82285def030033dadbd359fc75057f
DouglasCristiano <dcristiano.silva@outlook.com>
2023-10-25 17:30:28 -0300

    primeiro

---
+++ aula1.py
@@ -0,0 +1,9 @@
+#comentario aqui
+
+""""
+Docstring..... nao é considerado comentario
+"""
+
+print('Hello Word')
+print('Outra coisa')
+print(1 + 1)
---
+++ aula2.py
@@ -0,0 +1,6 @@
+# print('Hello', 123, 'Word', sep='*') # sep = separador
+# print('Hello', 456, 'Word')
+# print(    type(123)     )
+# print(    type('teste')     )
+print(    10 == 10     )
+print(    10 != 10     )
